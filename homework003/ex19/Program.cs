5f2117e13027729ac2e65f7aa224bc9245288284
lewe0fun <pakuln.yury@gmail.com>
2022-08-11 23:34:06 +0300

    3 домашка

---
+++ homework003/ex19/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 19.Напишите программу, которая принимает на вход пятизначное(и другое) число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+using static System.Console;
+WriteLine("Введте число");
+string digit=ReadLine();
+int diffDigitCounter = 0;
+for(int i=0;i<digit.Length;i++)
+{
+    if (digit[i] == digit[digit.Length-i-1])diffDigitCounter++;
+}
+if (diffDigitCounter == digit.Length) WriteLine("Это число палиндромом");
+else WriteLine($"Это число не палиндромом");
---
+++ homework003/ex21/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+**Задача 21**Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+using static System.Console;
+WriteLine("Ввекдите координату X1");
+int x1 = int.Parse(Console.ReadLine());
+WriteLine("Ввекдите координату Y1");
+int y1 = int.Parse(Console.ReadLine());
+WriteLine("Ввекдите координату Z1");
+int z1 = int.Parse(Console.ReadLine());
+WriteLine("Ввекдите координату X2");
+int x2 = int.Parse(Console.ReadLine());
+WriteLine("Ввекдите координату Y2");
+int y2 = int.Parse(Console.ReadLine());
+WriteLine("Ввекдите координату Z2");
+int z2 = int.Parse(Console.ReadLine());
+
+double c = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+WriteLine($"Рассстояние между этими точками равно: {c}");
