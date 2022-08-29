b80b9c4ddf9248748cf0f201f16c41644f4d6d0b
lewe0fun <pakuln.yury@gmail.com>
2022-08-29 15:57:09 +0300

    Methods in separate file

--- selftesting/sortArray/Program.cs
+++ selftesting/sortArray/Program.cs
@@ -1,40 +1,9 @@
-﻿using static System.Console;
-void swap(ref int a, ref int b)
-{
-int temp=a;
-a=b;
-b=temp;
-}
-void fillArray(int[]arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = new Random().Next(10,99);
-    }
-}
-void sortArray(int[] arr)
-{
-for (int i = 0; i < arr.Length-1; i++)
-{
-    for (int j = i+1; j < arr.Length; j++)
-    {
-        if(arr[j]<arr[i])swap(ref arr[j],ref arr[i]);
-    }
-}
-}
-void printArr(int[]arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Write($" {arr[i]}");
-        if((i!=0)&&(!Convert.ToBoolean(i%10)))Write("\n");
-    }Write("\n");
-}
+﻿using Methods;
 int[] arr ={12,32,35,9,10,999};
 int [] arr2 = new int[40];
-sortArray(arr);
-printArr(arr);
-fillArray(arr2);
-printArr(arr2);
-sortArray(arr2);
-printArr(arr2);
+mtd.sortArray(arr);
+mtd.printArr(arr);
+mtd.fillArray(arr2);
+mtd.printArr(arr2);
+mtd.sortArray(arr2);
+mtd.printArr(arr2);
